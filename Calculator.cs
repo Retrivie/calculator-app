a0ce4ae3d5bd2b0e7246e801be52e00d35d2a02f
Reychandra Atta <61732197+Retrivie@users.noreply.github.com>
2020-03-14 10:55:13 +0700

    Final v2.0

--- Calculator.cs
+++ Calculator.cs
@@ -1,23 +1,55 @@
-class Calculator
-{
-	static void Main(string[] args)
-	{
-	int a = 10;
-	int b = 6;
-	
-	Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
-	Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
-	
-	Console.WriteLine("\nTekan Sembarang key untuk keluar");
-	Console.ReadKey();
+class Calculator{
+	static void Main(string[] args){
+		Console.WriteLine("Progam Kalkulator\n\n");
+		
+		Console.WriteLine("1. Penambahan");
+		Console.WriteLine("2. Pengurangan");
+		Console.WriteLine("3. Perkalian");
+		Console.WriteLine("4. Pembagian");
+		int pilihan;
+		int a, b ;
+		
+		Console.WriteLine("\nSilahkan Pilih Menu : ");
+		pilihan =  Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("\nInput Nilai a : ");
+		a =  Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("\nInput Nilai b: ");
+		b =  Convert.ToInt32(Console.ReadLine());
+		
+		switch(pilihan){
+			case 1:
+				Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a,b, Penambahan(a, b));
+				break;
+			case 2:
+				Console.WriteLine("Hasil Pengurangan: {0} + {1} = {2}", a,b, Pengurangan(a, b));
+				break;
+			case 3:
+				Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a,b, Perkalian(a, b));
+				break;
+			case 4:
+				Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a,b, Pembagian(a, b));
+				break;
+			default:
+				Console.WriteLine("Error");
+				break;
+		}
+		
+
+		Console.WriteLine("\nPress any key to continue");
+		Console.ReadKey();
 	}
-	static int Penambahan(int a, int b)
-	{
+	static int Penambahan(int a, int b){
 		return a + b;
 	}
-	
-	static int Penambahan(int a, int b)
-	{
+	static int Pengurangan(int a, int b){
 		return a - b;
+	}
+	static int Perkalian(int a, int b){
+		return a * b;
+	}
+	static int Pembagian(int a, int b){
+		return a / b;
 	}
 }
